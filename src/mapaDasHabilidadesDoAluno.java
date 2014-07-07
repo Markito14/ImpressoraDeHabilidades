b1d2137cb19fba210f945c57c4be0a2a78b8078b
Alisson Felipe Bezerra <alissonfelipe@gmail.com>
2014-07-07 16:55:34 -0300

    Classes correction
    Correction from initual setup, some classes
    removal/addition/modification

---
+++ src/mapaDasHabilidadesDoAluno.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class mapaDasHabilidadesDoAluno {
+
+    public HashMap habilidadesPorId; //key = id da habilidade, value = int[30]
+
+    public mapaDasHabilidadesDoAluno() {
+        habilidadesPorId = new HashMap();
+    }
+
+}
---
+++ src/mapaDasQuestoesDoAluno.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class mapaDasQuestoesDoAluno {
+
+    public HashMap respostasPorId; //key = ID_PROVA, value = int[45]
+
+    public mapaDasQuestoesDoAluno() {
+        respostasPorId = new HashMap();
+    }
+}
